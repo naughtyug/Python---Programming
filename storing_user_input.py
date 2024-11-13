25f068a13de1922b1c2cec64e5692ce4424f5317
Balirwa Alvin Daniel <sanyukalvin@gmail.com>
2024-11-13 11:14:17 +0300

    Storing Input from a user

---
+++ storing_user_input.py
@@ -0,0 +1,3 @@
+User = input("Hello! May you please enter your name here: ")
+user_age = int(input("Once more, may you please enter your age here: "))
+print(f"Hello {User}, you are {user_age} years old.")
