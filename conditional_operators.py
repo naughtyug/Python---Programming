2d48fccb67cfa2360b1fbfed9de5677050e5d79d
Balirwa Alvin Daniel <sanyukalvin@gmail.com>
2024-11-13 11:10:48 +0300

    Working with Conditional Operators

---
+++ conditional_operators.py
@@ -0,0 +1,9 @@
+number_x = int(input("Enter a value for number_x: "))
+number_y = int(input("Enter a value for number_y: "))
+
+if number_x > number_y:
+    print("number_x is greater than number_y")
+elif number_x < number_y:
+    print("number_x is less than number_y")
+else:
+    print("number_x is equal to number_y")
